ab3008f6ac7ee13f5d5c9664c7684013c35b5a7f
Pablo <paul.ladiv123@gmail.com>
2019-02-18 15:41:27 +0000

    add encrypted password digest to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
 
+    before_save { email.downcase}
+
     validates :name, presence: true
     validates :email, presence: true, uniqueness: {case_sensitive: false}
-
-    
+    validates :password, presence: true, length: { minimum: 6} 
+    has_secure_password
 
 end
