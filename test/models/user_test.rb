fd2a3d7270de0252e1bd69fea93b9720b0f317bf
Pablo <paul.ladiv123@gmail.com>
2019-02-18 14:45:06 +0000

    add presence validation for email and name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,17 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "name should be present" do   
+    @user.name = "    "
+    assert_not @user.valid?
+  end 
+
+  test "email should be present" do    
+    @user.email = "    "
+    assert_not @user.valid?
+
+  end 
+
+
+
 end
