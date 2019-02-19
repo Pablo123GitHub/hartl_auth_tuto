94d078b99d72fe1b439604dd2381fb6f536dea86
Pablo <paul.ladiv123@gmail.com>
2019-02-19 15:01:35 +0000

    add first integration test for the signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
 
